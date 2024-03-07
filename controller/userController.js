52f98b1a0e4fec6f5a2d50c9622a1a2f924c8aff
rishalism <rishalmuhammedup@gmail.com>
2024-03-08 01:39:33 +0530

    updated controller and created a new middleware and added error handling middlewares and installed express error handler

--- controller/userController.js
+++ controller/userController.js
@@ -1,1 +1,47 @@
-// Remove this line after git pull
+const bcrypt = require('bcrypt')
+const users = require('../models/userModel')
+const asyncHandler = require('express-async-handler');
+const { render } = require('ejs');
+
+// use async instead  of try catch 
+
+
+
+const securepassword = asyncHandler(async (password) => {
+    const passwordHash = await bcrypt.hash(password, 10);
+    return passwordHash;
+})
+
+
+
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password, mobile } = req.body;
+    const profile = req.file.filename;
+    const hashedPassword = securepassword(password)
+    const isUserExists = await users.findOne({ email });
+    if (isUserExists) {
+        res.status(400)
+        throw new Error('this email is already exist');
+    } else {
+        const newUser = await new users({
+            name,
+            email,
+            hashedPassword,
+            profile,
+            mobile
+        })
+        const userDataSaved = await newUser.save()
+        if (userDataSaved) {
+            res.redirect('/')
+        } else {
+            throw new Error('FAILED TO SIGNUP')
+        }
+    }
+})
+
+
+module.exports = {
+    registerUser
+}
---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,21 @@
+const notFound = (req,res,next) =>{
+    const error = new Error(`Not Found ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+  }
+  
+  const errorHandler = (err,req,res,next) =>{
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    const message = err.message
+  
+    if(err.name === 'CastError' && err.kind === 'ObjectId'){
+      statusCode = 404 ;
+      message = 'Resource not found';
+    }
+  
+    res.status(statusCode).json({
+      message,
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+  }
+  
