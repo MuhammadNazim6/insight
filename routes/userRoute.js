a53c65c1d113b82882fecd9f5bad78eb82f4fd58
sabith <sabithmuhammed136@gmail.com>
2024-03-08 14:29:55 +0530

    merge conflicts fixed

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,21 +2,15 @@ const express = require("express")
 const userController = require('../controller/userController.js')
 const visionController = require('../controller/visionController.js')
 const userRoute = express.Router()
+const imageUpload = require("../middleware/multerMiddleware")
 
-<<<<<<< HEAD
+
+userRoute.get('/register',userController.showRegister)
 userRoute.post('/register', userController.registerUser)
 userRoute.post('/login', userController.loginUser)
 userRoute.post('/logout', userController.logoutUser)
 userRoute.post('/create-vision', visionController.createVision)
 userRoute.post('/upvote', visionController.upvoteVision)
-=======
-const imageUpload = require("../middleware/multerMiddleware")
-
-userRoute.get('/register',userController.showRegister)
-userRoute.post('/register',imageUpload,userController.registerUser)
-userRoute.post('/login',userController.loginUser)
-userRoute.post('/logout',userController.logoutUser)
->>>>>>> sabith
 
 
 module.exports = userRoute
