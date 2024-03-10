ff7985656b0822f76e5d33c32b266f3c46b51526
sabith <sabithmuhammed136@gmail.com>
2024-03-10 23:44:39 +0530

    bug fix

--- assets/script/chat.js
+++ assets/script/chat.js
@@ -1,24 +1,27 @@
 let visionId = null;
 
-const sendChat=async(e)=>{
+const sendChat = async (e) => {
     try {
         e.preventDefault();
-        const message = e.target.elements.chat.value
-        const data = await fetchData("POST","/send-message",{message,visionId})
-        e.target.elements.chat.value = ""
-        handleMessages(data.messageDetails,"right");
-
+        const message = e.target.elements.chat.value;
+        const data = await fetchData("POST", "/send-message", {
+            message,
+            visionId,
+        });
+        e.target.elements.chat.value = "";
+        handleMessages(data.messageDetails, "right");
     } catch (error) {
-        toast("error",error.message)
+        toast("error", error.message);
     }
-}
+};
 
-
-const handleMessages = (messageDetails,type) => {
+const handleMessages = (messageDetails, type) => {
     const chatHistory = document.querySelector(`[data-chat-history]`);
     const comment = createChatElement(messageDetails, type);
     chatHistory?.appendChild(comment);
-    chatHistory.scrollTop =chatHistory.scrollHeight
+    if (chatHistory) {
+        chatHistory.scrollTop = chatHistory?.scrollHeight;
+    }
 };
 
 const createChatElement = (messageDetails, type) => {
@@ -32,47 +35,57 @@ const createChatElement = (messageDetails, type) => {
 
     return div;
 };
-const showInterest = () =>{
-    document.querySelector(`[data-interest]`).classList.remove('hide')
-    document.querySelector(`[data-chat-show]`).classList.add('hide')
-}
-const showChatDiv = ()=>{
-    document.querySelector(`[data-interest]`).classList.add('hide')
-    document.querySelector(`[data-chat-show]`).classList.remove('hide')
-}
-const showIndividualChat = async (id)=>{
-    visionId = id
+const showInterest = () => {
+    document.querySelector(`[data-interest]`).classList.remove("hide");
+    document.querySelector(`[data-chat-show]`).classList.add("hide");
+};
+const showChatDiv = () => {
+    document.querySelector(`[data-interest]`).classList.add("hide");
+    document.querySelector(`[data-chat-show]`).classList.remove("hide");
+};
+const showIndividualChat = async (id) => {
+    visionId = id;
 
+    document.querySelector(`[data-homescreen]`).classList.add("hide");
+    document.querySelector(`[data-individual]`).classList.remove("hide");
+    const history = document.querySelector(`[data-chat-history]`);
+    const data = await fetchData("GET", `/get-message/${id}`);
+    if (data.status == "success") {
+        const vision = data?.vision;
+        if (vision) {
+            const members = vision.interested.reduce(
+                (acc, value) => acc + value.userId.name + ", ",
+                ""
+            );
+            document.querySelector(`[chat-title-chat]`).textContent =
+                vision.title;
 
-    document.querySelector(`[data-homescreen]`).classList.add('hide')
-    document.querySelector(`[data-individual]`).classList.remove('hide')
-    const history = document.querySelector(`[data-chat-history]`)
-    const data = await fetchData("GET",`/get-message/${id}`)
-    if(data.status=="success"){
-        const vision = data?.vision
-        if(vision){
-            const members = vision.interested.reduce((acc,value)=>acc+value.userId.name +", ","")
-            document.querySelector(`[chat-title-chat]`).textContent=vision.title
-
-            document.querySelector(`[chat-member-chat]`).textContent = members
+            document.querySelector(`[chat-member-chat]`).textContent = members;
         }
-        data?.messages?.forEach(element => {
-            const date = new Date(element.sendAt)
-            const div  = document.createElement('div')
-            div.setAttribute('class',element.userId._id == data.userId?"chat-right":"chat-left")
+        data?.messages?.forEach((element) => {
+            const date = new Date(element.sendAt);
+            const div = document.createElement("div");
+            div.setAttribute(
+                "class",
+                element.userId._id == data.userId ? "chat-right" : "chat-left"
+            );
             div.innerHTML = `<p class="chat-text">
                              ${element.message}
                             </p>
                             <div class="chat-details">
                                 <span>${element.userId.name}</span
-                                ><span>${date.toLocaleDateString()+" "+date.toLocaleDateString()}</span>
+                                ><span>${
+                                    date.toLocaleDateString() +
+                                    " " +
+                                    date.toLocaleDateString()
+                                }</span>
                             </div>
-            `
-            history.appendChild(div)
+            `;
+            history.appendChild(div);
         });
     }
-}
-const gotoHomescreen=()=>{
-    document.querySelector(`[data-homescreen]`).classList.remove('hide')
-    document.querySelector(`[data-individual]`).classList.add('hide')
-}
+};
+const gotoHomescreen = () => {
+    document.querySelector(`[data-homescreen]`).classList.remove("hide");
+    document.querySelector(`[data-individual]`).classList.add("hide");
+};
